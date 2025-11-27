04af0d45c595b80f76a96c2a43713444e293ec28
Kate <slastina0821@1511.ru>
2025-11-27 23:19:57 +0300

    full stack logic

--- steck_logic.c
+++ steck_logic.c
@@ -3,12 +3,15 @@
 #include "steck_logic.h"
 #include "sys_funcs.h"
 
+
 int push(element** se, int value){
-    if (se ==NULL){
+    if (se == NULL){
+        printf("Отсутствует указатель на stack_end\n");
         return 1;
     }
     element* new_el=malloc(sizeof(element));
     if(new_el==NULL){
+        printf("Новый элемент не создался\n");
         return 1;
     }
     new_el->data = value;
@@ -16,13 +19,16 @@ int push(element** se, int value){
     *se = new_el;
     return 0;
 }
+
+
 int pop(element** se){
-    if (se==NULL){
+    if (se == NULL){
+        printf("Отсутствует указатель на stack_end\n");
         return 1;
     }
-    if(*se==NULL){
-        printf("Nothing in stack");
-        return 0;
+    if(*se == NULL){
+        printf("В стеке ничего нет\n");
+        return 2;
     }
     element* cur_element = *se;
     *se = (*se)->next;
@@ -30,12 +36,32 @@ int pop(element** se){
     return 0;
 
 }
+
+
+int clear(element** se){
+    int check = 0;
+    if (se == NULL){
+        printf("Отсутствует указатель на stack_end\n");
+        return 1;
+    }
+    while(*se != NULL){
+        check = pop(se);
+        if(check != 0){
+            return check;
+        }
+    }
+    return 0;
+
+}
+
+
 int show(element** se){
-    if (se==NULL){
+    if (se == NULL){
+        printf("Отсутствует указатель на stack_end\n");
         return 1;
     }
     if(*se==NULL){
-        printf("Nothing in stack");
+        printf("В стеке ничего нет\n");
         return 0;
     }
     element* cur_element = *se;
@@ -51,7 +77,7 @@ int main_logic(){
     int choice = 0, value = 0, check = 0, rand_num = 0;
     char value_text[] = "Введи значение для вставки\n>", count_text[] = "Введи количество значений для вставки\n>"; 
     element*  stack_end=NULL;
-    while(choice != 5){
+    while(choice != 6){
         printf("[1] - добавить число в стек \n");
         printf("[2] - заполнить стек рандомными элементами \n");
         printf("[3] - удалить число из стека \n");
@@ -66,18 +92,22 @@ int main_logic(){
                 check = push(&stack_end, value);
                 if(check == 0){
                     printf("Число добавлено.");
-                    clear_input();
-                    clear_screen();
                 }
                 else{
-                    //destroy
+                    clear(&stack_end);
                     return 1;
                 }
                 break;
             case 2:
-                value_input(&value, value_text);
+            value = 1000000;
+                while(value < 1 || value > 100000){
+                    value_input(&value, count_text);
+                    if(value < 1 || value > 100000){
+                        printf("Значение должно быть в промежутке от 1 до 10^5\n");
+                    }
+                }
                 for(int i = 0; i < value; i++){
-                    rand_num = rand();
+                    rand_num = rand()%1000;
                     check = push(&stack_end, rand_num);
                     if(check != 0){
                         break;
@@ -85,17 +115,50 @@ int main_logic(){
                 }
                 if(check == 0){
                     printf("Числа добавлены.");
-                    clear_input();
-                    clear_screen();
                 }
                 else{
-                    //destroy
+                    clear(&stack_end);
+                    return 1;
+                }
+                break;
+            case 3:
+                check = pop(&stack_end);
+                if(check == 0){
+                    printf("Число удалено.");
+                }
+                else if(check == 1){
+                    clear(&stack_end);
+                    return 1;
+                }
+                break;
+            case 4:
+                check = clear(&stack_end);
+                if(check == 1){
+                    return 1;
+                }
+            case 5:
+                check = show(&stack_end);
+                if(check != 0){
+                    clear(&stack_end);
+                    return 1;
+                }
+                break;
+            case 6:
+                check = clear(&stack_end);
+                if(check == 0){
+                    printf("Стек удален. Завершение работы программы...");
+                }
+                else{
+                    clear(&stack_end);
                     return 1;
                 }
                 break;
             default:
+                printf("Такого варианта нема...");
                 break;
         }
+        clear_input();
+        clear_screen();
 
     }
     return 0;
